afd46d652a4a43d5351cf178d3c66358ae051b62
solacce <ikumapayisolace@gmail.com>
2022-08-26 18:48:44 +0100

    Created HTML,CSS and JS

---
+++ index.js
@@ -0,0 +1,94 @@
+function info(time) {
+  let date = now.getDate();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let year = now.getFullYear();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+
+  h2.innerHTML = `${month} ${date}, ${year}`;
+  h3.innerHTML = `${day}, ${hours}:${minutes}`;
+}
+info();
+
+function showPosition(position) {
+  let ApiKey = "073856d4c00a8e049aa1d750c90eefe5";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let ApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${ApiKey}`;
+
+  axios.get(ApiUrl).then(showWeather);
+}
+function showWeather(response) {
+  let temp = document.querySelector("#temp");
+  let temperature = Math.round(response.data.main.temp);
+  temp.innerHTML = `${temperature}`;
+  let h4 = document.querySelector("h4");
+  h4.innerHTML = `${response.data.name}`;
+}
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", getCurrentPosition);
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#country").innerHTML = response.data.sys.country;
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function showCity(city) {
+  let ApiKey = "073856d4c00a8e049aa1d750c90eefe5";
+  let ApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}&units=metric`;
+
+  axios.get(ApiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  showCity(city);
+}
+
+let searchForm = document.querySelector("#city-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+showCity("Lagos");
