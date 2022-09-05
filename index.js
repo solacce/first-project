95272b33d29cbf5188fc2b1a7cc6b17734da2de7
solacce <ikumapayisolace@gmail.com>
2022-09-05 10:53:44 +0100

    temperature conversion

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function showPosition(position) {
   axios.get(ApiUrl).then(showWeather);
 }
 function showWeather(response) {
-  let temp = document.querySelector("#temp");
+  let temp = document.querySelector("#temperature");
   let temperature = Math.round(response.data.main.temp);
   temp.innerHTML = `${temperature}`;
   let h4 = document.querySelector("h4");
@@ -66,9 +66,10 @@ button.addEventListener("click", getCurrentPosition);
 
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temp").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  celsiusTemperature = response.data.main.temp;
+
+  document.querySelector("#temperature").innerHTML =
+    Math.round(celsiusTemperature);
   document.querySelector("#country").innerHTML = response.data.sys.country;
 
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
@@ -97,3 +98,31 @@ let searchForm = document.querySelector("#city-form");
 searchForm.addEventListener("submit", handleSubmit);
 
 showCity("Lagos");
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
