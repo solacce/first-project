993ec12241050a7dd536a3cf9db4f798558a12fc
solacce <ikumapayisolace@gmail.com>
2022-09-14 01:59:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ function info(time) {
   h3.innerHTML = `${day}, ${hours}:${minutes}`;
 }
 info();
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -91,11 +92,12 @@ function getCurrentPosition(event) {
 }
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "073856d4c00a8e049aa1d750c90eefe5";
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
 
   let apiUrl = `https://api.openweather.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeatherCondition(response) {
@@ -172,4 +174,3 @@ button.addEventListener("click", getCurrentPosition);
 
 let searchForm = document.querySelector("#city-form");
 searchForm.addEventListener("submit", handleSubmit);
-displayForecast();
